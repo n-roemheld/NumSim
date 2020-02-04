27e2d670b098ebe76b826eb283a5c0572c0c12ec
Marc <marcp98@gmx.de>
2020-02-04 13:47:32 +0100

    fixed bug in MGGrid with size

--- src/MGGrid.cpp
+++ src/MGGrid.cpp
@@ -5,15 +5,15 @@
 // Use to restrict Staggered Grid to MGGrid on finest level
 MGGrid::MGGrid(std::array< int, 2> nCells, std::array< double, 2> meshWidth, FieldVariable p, FieldVariable rhs)
 : nCells_(nCells), meshWidth_(meshWidth), p_(p), rhs_(rhs),
-  resVec_({nCells[0]+2, nCells[1]+2}, {-.5*meshWidth_[0], -.5*meshWidth_[1]}, meshWidth_)
+  resVec_({nCells_[0]+2, nCells_[1]+2}, {-.5*meshWidth_[0], -.5*meshWidth_[1]}, meshWidth_)
 {};
 
 // Takes values of a finer grid and returns the next-coarser mggrid
 MGGrid::MGGrid(std::array< int, 2> nCells, std::array< double, 2> meshWidth)
 : nCells_({nCells[0]/2, nCells[1]/2}), meshWidth_({meshWidth[0]*2, meshWidth[1]*2}),
-  p_({nCells[0]+2, nCells[1]+2}, {-.5*meshWidth_[0], -.5*meshWidth_[1]}, meshWidth_),
-  resVec_({nCells[0]+2, nCells[1]+2}, {-.5*meshWidth_[0], -.5*meshWidth_[1]}, meshWidth_),
-  rhs_({nCells[0]+2, nCells[1]+2}, {-.5*meshWidth_[0], -.5*meshWidth_[1]}, meshWidth_)
+  p_({nCells_[0]+2, nCells_[1]+2}, {-.5*meshWidth_[0], -.5*meshWidth_[1]}, meshWidth_),
+  resVec_({nCells_[0]+2, nCells_[1]+2}, {-.5*meshWidth_[0], -.5*meshWidth_[1]}, meshWidth_),
+  rhs_({nCells_[0]+2, nCells_[1]+2}, {-.5*meshWidth_[0], -.5*meshWidth_[1]}, meshWidth_)
 {};
 
 const std::array< double, 2 > MGGrid::meshWidth() const
